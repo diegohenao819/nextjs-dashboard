6e7f8bf85fe3d1ed112429aa9c5518691b97d3fa
diegohenao819 <diegohenao819@gmail.com>
2024-10-06 17:03:54 -0500

    Now what?

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -77,6 +77,6 @@ export async function deleteInvoice(id: string) {
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    return { message: 'Database Error: Failed to Delete Invoice. Now what?' };
   }
 }
